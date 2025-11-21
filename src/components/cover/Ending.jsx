e788b5fa269e508d3b6db52eee0fabc7c7148920
DESKTOP-LT1NPT5\samsung <baekjw@uracle.co.kr>
2025-11-21 17:28:32 +0900

    outro image mod

--- src/components/cover/Ending.jsx
+++ src/components/cover/Ending.jsx
@@ -2,12 +2,13 @@ import ImgCanvas from "@/components/ImgCanvas";
 
 export default function Ending({ wedding_data, imgUrl }) {
     const ending = wedding_data?.ending?.split('\n');
+    const ending_img = wedding_data?.outro || imgUrl;
     return (
         <div id="ending" className="w-full relative rounded-b-3xl overflow-hidden">
             <h5 className="z-10 w-full pt-15 text-white absolute text-6xl leading-snug px-2 font-loving text-center">
                 <p>Thank You</p>
             </h5>
-            <ImgCanvas imgUrl={imgUrl} type="tuliip" />
+            <ImgCanvas imgUrl={ending_img} type="tuliip" />
             <div className="text-base w-full absolute bottom-20 z-10 text-center px-8 space-y-4 leading-7 font-saeum text-xl">
                 {ending && ending.map((e, i) => (
                     e.length > 0 ? (
